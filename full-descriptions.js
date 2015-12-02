7fe598a09b8bd14d7881449625b12d252c7f76df
Alex Usertesting <support@usertesting.com>
2015-12-02 10:05:10 -0800

    Stop polling constantly so that Trello dropdowns and things can stay open.

--- full-descriptions.js
+++ full-descriptions.js
@@ -1,3 +1,7 @@
 setInterval( function() {
-  document.getElementsByClassName('description-content-fade-button-text')[0].click();
+  var descButton = document.getElementsByClassName('description-content-fade-button-text')[0];
+  if( descButton && ! descButton.classList.contains('full-desc-clicked') ) {
+    descButton.click();
+    descButton.classList.add('full-desc-clicked');
+  }
 }, 1000 );
